5069af9c7aece6bca55104779497af1935a89ac4
Komron Mukhamadulloev <disqrl@yandex.ru>
2023-03-26 01:30:26 +0300

    first sync

---
+++ TicTacToeAI/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace TicTacToeAI
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A1.Location = new System.Drawing.Point(43, 36);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A2.Location = new System.Drawing.Point(149, 36);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A3.Location = new System.Drawing.Point(255, 36);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B3.Location = new System.Drawing.Point(255, 142);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B2.Location = new System.Drawing.Point(149, 142);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B1.Location = new System.Drawing.Point(43, 142);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C3.Location = new System.Drawing.Point(255, 248);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C2.Location = new System.Drawing.Point(149, 248);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C1.Location = new System.Drawing.Point(43, 248);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 380);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe - Player 0 : 0 AI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+    }
+}
+
---
+++ TicTacToeAI/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeAI
+{
+    public partial class TicTacToe : Form
+    {
+        private string player = "X";
+        private string computer = "O";
+        private int count = 0;
+        private int playerWin = 0;
+        private int AIwin = 0;
+        private Random random = new Random();
+        public TicTacToe()
+        {
+            InitializeComponent();
+            ResetGame();
+        }
+
+        private void changeText()
+        {
+            this.Text = $"TicTacToeAI - Player {playerWin}:{AIwin} AI";
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Text == "")
+            {
+                button.Text = player;
+                count++;
+                if (IsWinner(player))
+                {
+                    MessageBox.Show(player + " Wins!", "Congratulations");
+                    playerWin++;
+                    changeText();
+                    ResetGame();
+                }
+                else if (count == 9)
+                {
+                    MessageBox.Show("It's a draw!", "Game Over");
+                    ResetGame();
+                }
+                else
+                {
+                    player = (player == "X") ? "O" : "X";
+                    if (player == computer)
+                    {
+                        ComputerTurn();
+                    }
+                }
+            }
+        }
+
+        private void ComputerTurn()
+        {
+            Button button;
+            int index;
+
+            // If this is the first turn, choose a random button
+            if (count == 1)
+            {
+                if (random.Next(10) <= 3 && B2.Text == "") // 30% chance to choose center
+                {
+                    button = B2;
+                }
+                else // 70% chance to choose a random button
+                {
+                    index = random.Next(8);
+                    button = GetEmptyButtons().ElementAt(index);
+                }
+            }
+            else // Otherwise, use the MiniMax algorithm to choose the best move
+            {
+                int bestScore = int.MinValue;
+                button = null;
+                foreach (Button b in GetEmptyButtons())
+                {
+                    b.Text = "O";
+                    int score = MiniMax("X", 0, false);
+                    b.Text = "";
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        button = b;
+                    }
+                }
+            }
+
+            button.Text = "O";
+            count++;
+            if (IsWinner("O"))
+            {
+                MessageBox.Show("Computer wins!");
+                AIwin++;
+                changeText();
+                ResetGame();
+            }
+            else if (count == 9)
+            {
+                MessageBox.Show("Draw!");
+                ResetGame();
+            }
+            else
+            {
+                player = "X";
+            }
+        }
+
+        private int MiniMax(string player, int depth, bool maximizingPlayer)
+        {
+            if (IsWinner("X"))
+            {
+                return -10 + depth;
+            }
+            else if (IsWinner("O"))
+            {
+                return 10 - depth;
+            }
+            else if (GetEmptyButtons().Count() == 0)
+            {
+                return 0;
+            }
+
+            if (maximizingPlayer)
+            {
+                int bestScore = int.MinValue;
+                foreach (Button button in GetEmptyButtons())
+                {
+                    button.Text = player;
+                    int score = MiniMax(player == "X" ? "O" : "X", depth + 1, false);
+                    button.Text = "";
+                    bestScore = Math.Max(score, bestScore);
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                foreach (Button button in GetEmptyButtons())
+                {
+                    button.Text = player;
+                    int score = MiniMax(player == "X" ? "O" : "X", depth + 1, true);
+                    button.Text = "";
+                    bestScore = Math.Min(score, bestScore);
+                }
+                return bestScore;
+            }
+        }
+
+
+
+        private bool IsWinner(string player)
+        {
+            // Check rows
+            if ((A1.Text == player && A2.Text == player && A3.Text == player) ||
+                (B1.Text == player && B2.Text == player && B3.Text == player) ||
+                (C1.Text == player && C2.Text == player && C3.Text == player))
+            {
+                return true;
+            }
+
+            // Check columns
+            if ((A1.Text == player && B1.Text == player && C1.Text == player) ||
+                (A2.Text == player && B2.Text == player && C2.Text == player) ||
+                (A3.Text == player && B3.Text == player && C3.Text == player))
+            {
+                return true;
+            }
+
+            // Check diagonals
+            if ((A1.Text == player && B2.Text == player && C3.Text == player) ||
+                (A3.Text == player && B2.Text == player && C1.Text == player))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private IEnumerable<Button> GetEmptyButtons()
+        {
+            return Controls.OfType<Button>().Where(button => button.Text == "");
+        }
+
+        private void ResetGame()
+        {
+            foreach (Button button in Controls.OfType<Button>())
+            {
+                button.Text = "";
+            }
+            count = 0;
+            player = "X";
+        }
+    }
+}
